7491a3f85c54d447c16a1481c13d4c4269b1224c
HenryTong <taksantong@gmail.com>
2023-06-13 16:46:05 -0300

    add call to generated and download the generated pdf report

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,3 +13,31 @@ if [[ -z "${TM_API_URL}" ]]; then
 fi
 
 npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} snapshot create-analyze --repo-id ${1} --file ${2}
+
+# overview of process: create a snapshot with the input source, analyze it, then download the result as a pdf report
+
+echo "Create a snapshot"
+COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} snapshot create --repo-id ${1} --file ${2} --output=json)
+
+# attempt parse json output from previous command, show error in console if failed to parse
+SNAPSHOT_ID=$(jq '.ID' <<< "$COMMAND_OUTPUT") || echo "Cannot parse output from command because: $COMMAND_OUTPUT"
+if [[ ! -z $SNAPSHOT_ID && $SNAPSHOT_ID != "null" ]]; then
+    echo "Created Snapshot Id for analysis: $SNAPSHOT_ID"
+    COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} snapshot analyze --snapshot-id=${SNAPSHOT_ID} --output=json)
+    # attempt parse json output from previous command, show error in console if failed to parse
+    ANA_ID=$(jq '.id' <<< "$COMMAND_OUTPUT")
+    if [[ ! -z $ANA_ID && $ANA_ID != "null" ]]; then
+        echo "Requesting report for generated Analysis Id: $ANA_ID"
+        COMMAND_OUTPUT=$(npx @testmachine.ai/cli -t ${TM_TOKEN_KEY} analyses report --analysis-id=${ANA_ID} --output=json)
+        # attempt parse json output from previous command, show error in console if failed to parse
+        DOWNLOAD_URL=$(jq '.downloadURLOfGeneratedReport' <<< "$COMMAND_OUTPUT")
+        if [[ ! -z $DOWNLOAD_URL && $DOWNLOAD_URL != "null" ]]; then
+            # remove double quotes from beginning and end of download url
+            temp="${DOWNLOAD_URL%\"}"
+            DOWNLOAD_URL="${temp#\"}"
+            # download file
+            echo "Downloading generated report PDF download URL: $DOWNLOAD_URL"
+            wget -O action_work_result_report.pdf ${DOWNLOAD_URL}
+        fi
+    fi
+fi
